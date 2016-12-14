c3d25cdde7d2db1957d1ae58079d6a2f98dbba73
John Warren <jswarren@us.ibm.com>
2016-12-14 13:54:23 -0500

    Add doc file for package description

---
+++ rules/doc.go
@@ -0,0 +1,5 @@
+// Package rules is a rules engine for etcd. Specified functions are triggered
+// whenever a specified rule is satisfied.  Rules can contain gin-style attribute
+// specifiers so that classes of keys are matched as opposed to having to specify
+// each node key separately.
+package rules
