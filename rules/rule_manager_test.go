35b67fe78a306f851acf141a680470161f7a5731
Olivia Golden <olivia.golden@ibm.com>
2025-05-08 15:14:19 -0500

    test

--- rules/rule_manager_test.go
+++ rules/rule_manager_test.go
@@ -21,13 +21,13 @@ func TestRuleManager(t *testing.T) {
 		rule3, err3 := NewEqualsLiteralRule("/this/is/:a", nil)
 		assert.NoError(t, err3)
 		rm.addRule(rule3, opts)
-		rules := rm.getStaticRules("/this/is/a/rule", nil)
+		rules, _ := rm.getStaticRules("/this/is/a/rule", nil)
 		assert.Equal(t, 1, len(rules))
 		for r, index := range rules {
 			assert.Equal(t, 0, index)
 			assert.True(t, r.satisfiable("/this/is/a/rule", nil))
 		}
-		rules = rm.getStaticRules("/nothing", nil)
+		rules, _ = rm.getStaticRules("/nothing", nil)
 		assert.Equal(t, 0, len(rules))
 	}
 }
@@ -83,32 +83,31 @@ func TestGetPrioritizedPrefixes(t *testing.T) {
 	rm.addRule(rule1, makeRuleOptions(Priority(100)))
 
 	// overlapping, longer prefix with a higher priority
-	rule5, err5 := NewEqualsLiteralRule("/this/is/overlapping/:a", nil)
-	assert.NoError(t, err5)
-	rm.addRule(rule5, makeRuleOptions(Priority(300)))
+	rule2, err2 := NewEqualsLiteralRule("/this/is/overlapping/:a", nil)
+	assert.NoError(t, err2)
+	rm.addRule(rule2, makeRuleOptions(Priority(300)))
 
 	// Final priority - 200
-	// same prefix as earlier, largest one should be considered
-	rule6, err6 := NewEqualsLiteralRule("/these/are/:a/ruleset", nil)
-	assert.NoError(t, err6)
-	rm.addRule(rule6, makeRuleOptions(Priority(200)))
+	rule3, err3 := NewEqualsLiteralRule("/these/are/:a/ruleset", nil)
+	assert.NoError(t, err3)
+	rm.addRule(rule3, makeRuleOptions(Priority(200)))
 
 	// same prefix as earlier, largest one should be considered
-	rule3, err3 := NewEqualsLiteralRule("/these/are/:a", nil)
-	assert.NoError(t, err3)
-	rm.addRule(rule3, makeRuleOptions(Priority(50)))
+	rule4, err4 := NewEqualsLiteralRule("/these/are/:a", nil)
+	assert.NoError(t, err4)
+	rm.addRule(rule4, makeRuleOptions(Priority(50)))
 
 	// Final priority - 0
 	// no priority, should be last
-	rule2, err2 := NewEqualsLiteralRule("/that/is/:a/nother", nil)
-	assert.NoError(t, err2)
-	rm.addRule(rule2, makeRuleOptions())
+	rule5, err5 := NewEqualsLiteralRule("/that/is/:a/nother", nil)
+	assert.NoError(t, err5)
+	rm.addRule(rule5, makeRuleOptions())
 
 	// Final priority - 100
 	// third tier priority rule
-	rule4, err4 := NewEqualsLiteralRule("/this/one/is/:a", nil)
-	assert.NoError(t, err4)
-	rm.addRule(rule4, makeRuleOptions(Priority(100)))
+	rule6, err6 := NewEqualsLiteralRule("/this/one/is/:a", nil)
+	assert.NoError(t, err6)
+	rm.addRule(rule6, makeRuleOptions(Priority(100)))
 
 	assert.Equal(t, []string{"/this/is/", "/these/are/", "/this/one/is/", "/that/is/"}, rm.getPrioritizedPrefixes())
 }
@@ -121,9 +120,39 @@ func TestAddRuleCrawlerOnly(t *testing.T) {
 	rm.addRule(rule1, makeRuleOptions(Priority(100), CrawlerOnly()))
 
 	// overlapping, longer prefix with a higher priority
-	rule5, err5 := NewEqualsLiteralRule("/this/is/overlapping/:a", nil)
-	assert.NoError(t, err5)
-	rm.addRule(rule5, makeRuleOptions(Priority(300), CrawlerOnly()))
+	rule2, err2 := NewEqualsLiteralRule("/this/is/overlapping/:a", nil)
+	assert.NoError(t, err2)
+	rm.addRule(rule2, makeRuleOptions(Priority(300), CrawlerOnly()))
 
 	assert.True(t, assert.ObjectsAreEqual(map[string]ruleMgrRuleOptions{"/this/is/": {priority: 300, crawlerOnly: true}}, rm.prefixes))
+}
+
+func TestGetStaticRules(t *testing.T) {
+	rm := newRuleManager(map[string]constraint{}, false)
+
+	rule1, err1 := NewEqualsLiteralRule("/this/is/:a/rule", nil)
+	assert.NoError(t, err1)
+	rm.addRule(rule1, makeRuleOptions(Priority(100)))
+
+	rule2, err2 := NewEqualsLiteralRule("/this/is/overlapping/:a", nil)
+	assert.NoError(t, err2)
+	rm.addRule(rule2, makeRuleOptions(Priority(300)))
+
+	rule3, err3 := NewEqualsLiteralRule("/these/are/:a/ruleset", nil)
+	assert.NoError(t, err3)
+	rm.addRule(rule3, makeRuleOptions(Priority(50)))
+
+	rule4, err4 := NewEqualsLiteralRule("/these/are/:a/ruleset", nil)
+	assert.NoError(t, err4)
+	rm.addRule(rule4, makeRuleOptions(Priority(200)))
+
+	rule5, err5 := NewEqualsLiteralRule("/that/is/:a/nother", nil)
+	assert.NoError(t, err5)
+	rm.addRule(rule5, makeRuleOptions())
+
+	// The 200 priority rule, 3 rule added, should be first in
+	// the priority slice
+	index, priority := rm.getStaticRules("/these/are/a/ruleset", nil)
+	assert.Equal(t, 3, index[priority[0]])
+	assert.Equal(t, 2, index[priority[1]])
 }
